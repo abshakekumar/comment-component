6ce202ae31376926c25a3a17694af76573fc7671
abshakekumar <abhishek.mcs.du.2014@gmail.com>
2020-07-29 12:45:19 +0530

    Basic Todo Addition

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList :list="list" @add-todo="addTodo" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    TodoList
+  },
+  data() {
+    return {
+      list: [{ value: 'Hello', id: 1},
+      { value: 'Hello ererer', id: 2}, 
+      { value: 'Hello343434', id: 3}]
+    }
+    
+  },
+  methods: {
+    addTodo(data) {
+      console.log('Add todo : ', data);
+      this.list = [...this.list, {
+                    id: Math.random(),
+                    value: data
+                }]
+    }
   }
 }
 </script>
 
 <style>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+  "Roboto", "Oxygen", "Ubuntu", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji",
+  "Segoe UI Emoji", "Segoe UI Symbol";
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="todo-list-container">
+        <input 
+        v-model="inputVal"
+        @keyup.enter="hanleEnterClick"
+        type="text" />
+        <div v-for="item in list" :key="item.id">
+            {{item.value}}
+        </div>
+        Entered Val: {{inputVal}}
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'TodoList',
+    props: ['list'],
+    data() {
+        return {
+            inputVal: 'default'
+        }
+    },
+    methods:  {
+            hanleEnterClick: function () {
+                console.log('Handle Keypress');
+                this.$emit('add-todo', this.inputVal);
+                this.$nextTick(()=>{
+                    this.inputVal = ""
+                })
+            }
+    }
+}
+</script>
+
+<style scoped>
+.todo-list-container {
+    padding: 20px;
+}
+</style>
