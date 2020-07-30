5edc6c80e7ceb570eca45aa6bf83e1585a2cd96f
abshakekumar <abhishek.mcs.du.2014@gmail.com>
2020-07-30 10:22:11 +0530

    Push Branch to Github

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <TodoList :list="list" @add-todo="addTodo" />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
